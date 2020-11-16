88023ccba4b54bce7686dd529649acb5de60fc0b
Derek Dhammaloka <41344434+derektypist@users.noreply.github.com>
2020-11-16 16:00:48 +0000

    Update api.js

--- routes/api.js
+++ routes/api.js
@@ -16,6 +16,23 @@ let uri = process.env.DB;
 
 module.exports = function (app) {
 
+  mongoose.connect(uri, {useNewUrlParser:true, useUnifiedTopology:true});
+
+  let issueSchema = new mongoose.Schema({
+    issue_title: {type: String, required: true},
+    issue_text: {type: String, required: true},
+    created_by: {type: String, required: true},
+    assigned_to: String,
+    status_text: String,
+    open: {type: Boolean, required: true},
+    created_on: {type: Date, required: true},
+    updated_on: {type: Date, required: true},
+    project: String
+  });
+
+  let Issue = mongoose.model('Issue', issueSchema);
+  
+  
   app.route('/api/issues/:project')
   
     .get(function (req, res){
