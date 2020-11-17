70321aba47056b77b2f8e425d6e4e6322fa5dc83
Derek Dhammaloka <41344434+derektypist@users.noreply.github.com>
2020-11-17 10:21:32 +0000

    Update api.js

--- routes/api.js
+++ routes/api.js
@@ -48,17 +48,74 @@ module.exports = function (app) {
     
     .post(function (req, res){
       var project = req.params.project;
+      // Create Issue
+      if (!req.body.issue_title || !req.body.issue_text || !req.body.created_by) {
+        return res.json('Required fields missing from request');
+      }
       
+      let newIssue = new Issue({
+        issue_title: req.body.issue_title,
+        issue_text: req.body.issue_text,
+        created_by: req.body.created_by,
+        assigned_to: req.body.assigned_to || '',
+        status_text: req.body.status_text || '',
+        open: true,
+        created_on: new Date().toUTCString(),
+        updated_on: new Date().toUTCString(),
+        project: project
+      });
+
+      newIssue.save((error, savedIssue) => {
+        if (!error && savedIssue) {
+          return res.json(savedIssue);
+        }
+      });
     })
     
     .put(function (req, res){
       var project = req.params.project;
+      // Update Issue
+      let updateObject = {};
+      Object.keys(req.body).forEach((key) => {
+        if (req.body[key] !== '') {
+          updateObject[key] = req.body[key];
+        }
+      });
+
+      if (Object.keys(updateObject).length < 2) {
+        return res.json('no updated field sent');
+      }
+
+      updateObject['updated_on'] = new Date().toUTCString();
+      Issue.findByIdAndUpdate(
+        req.body._id,
+        updateObject,
+        {new: true},
+        (error, updatedIssue) => {
+          if (!error && updatedIssue) {
+            return res.json('successfully updated');
+          } else if (!updatedIssue) {
+            return res.json('could not update ' + req.body._id);
+          }
+        }
+      );
       
     })
     
     .delete(function (req, res){
       var project = req.params.project;
-      
+      var project = req.params.project;
+      if (!req.body._id) {
+        return res.json('id error');
+      }
+
+      Issue.findByIdAndRemove(req.body._id, (error, deletedIssue) => {
+        if (!error && deletedIssue) {
+          return res.json('deleted ' + deletedIssue.id);
+        } else if (!deletedIssue) {
+          return res.json('could not delete ' + req.body._id);
+        }
+      });
     });
     
 };
